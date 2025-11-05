3f08327199e2fa5cf4d505cb2841f879118e771b
cerine <nc_chir@esi.dz>
2025-11-05 15:07:05 +0100

    updating insert.py for /onthisday

--- insert.py
+++ insert.py
@@ -49,28 +49,97 @@ def insertterm(term , definition):
         "definition" : definition 
     }
     collection.insert_one(document)
+def insert_onthisday( day , month , info):
+    db = client.cyber_history
+    collection = db.onthisday
+    document = {
+        "month" : month ,
+        "day" : day ,
+        "info" : info
+    }
+    collection.insert_one(document)  
 
-i = input("What do u wanna do ? 0 for inserting events , 1 for inserting facts ")
-if(i == 0):
-    while True : 
-        name = input("Type the name of the fact : ")
-        fact = input("Type the fact : ")
-        question = input("Type the question : ")
-        insert_fact(name , question , fact )
-        print("Success !")
-else:
-    j = input("Do you want to insert a Hacker fact or a Cyber attack ? 0 for Hacker , 1 for cyber attack")
-    if(j == 0):
-        while True : 
-            name = input("Type the name of the fact : ")
-            fact = input("Type the fact : ")
-            question = input("Type the question : ")
-            insert_fact_hackers(name , question , fact )
-            print("Success !")
+while True:
+    print("\nChoose an option:")
+    print("0 = Insert a Hacker fact")
+    print("1 = Insert a Cybersecurity fact")
+    print("2 = Insert an Event")
+    print("3 = Insert a Term")
+    print("4 = Insert a Tip")
+    print("5 = Insert an 'On this day' fact")
+    print("q = Quit")
+
+    choice = input("Your choice: ")
+
+    if choice == "0":
+        while True:
+            name = input("Name of the fact: ")
+            question = input("Question: ")
+            fact = input("Fact: ")
+            insert_fact_hackers(name, question, fact)
+            print("Success!")
+            cont = input("Add another Hacker fact? (y/n): ")
+            if cont.lower() != "y":
+                break
+
+    elif choice == "1":
+        while True:
+            name = input("Name of the fact: ")
+            question = input("Question: ")
+            fact = input("Fact: ")
+            insert_fact_cyberattacks(name, question, fact)
+            print("Success!")
+            cont = input("Add another Cybersecurity fact? (y/n): ")
+            if cont.lower() != "y":
+                break
+
+    elif choice == "2":
+        while True:
+            name = input("Event name: ")
+            year = input("Year: ")
+            month = input("Month: ")
+            day = input("Day: ")
+            details = input("Details: ")
+            insert_event(name, year, month, day, details)
+            print("Success!")
+            cont = input("Add another Event? (y/n): ")
+            if cont.lower() != "y":
+                break
+
+    elif choice == "3":
+        while True:
+            term = input("Term: ")
+            definition = input("Definition: ")
+            insertterm(term, definition)
+            print("Success!")
+            cont = input("Add another Term? (y/n): ")
+            if cont.lower() != "y":
+                break
+
+    elif choice == "4":
+        while True:
+            title = input("Tip title: ")
+            details = input("Details: ")
+            inserttip(title, details)
+            print("Success!")
+            cont = input("Add another Tip? (y/n): ")
+            if cont.lower() != "y":
+                break
+
+    elif choice == "5":
+        while True:
+            day = input("Day: ")
+            month = input("Month: ")
+            info = input("Info/Event: ")
+            insert_onthisday(day, month, info)
+            print("Success!")
+            cont = input("Add another 'On this day' fact? (y/n): ")
+            if cont.lower() != "y":
+                break
+
+    elif choice.lower() == "q":
+        print("Exiting program.")
+        break
+
     else:
-        while True : 
-            name = input("Type the name of the fact : ")
-            fact = input("Type the fact : ")
-            question = input("Type the question : ")
-            insert_fact_cyberattacks(name , question , fact )
-            print("Success !")
+        print("Invalid choice, try again.")
